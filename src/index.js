17057c885edad88f5de47e7ee882d4bdc0a4bb2e
John <megaman5249@gmail.com>
2021-12-22 13:28:16 -0500

    Add drop down logic and commit dist

--- src/index.js
+++ src/index.js
@@ -1,24 +1,79 @@
 import cityObject from "./city.list.json";
 const APIKey = "098aad25ae5f2e1c06ba7db2163ed759";
 
-const cityList = Object.values(cityObject);
-const countryOptions = cityList.reduce((list, city) => {
-  if (!list.includes(city.country)) {
-    list.push(city.country);
-  }
-  return list;
-}, []);
-const sortedCountryOptions = countryOptions.sort((a, b) => (a > b ? 1 : -1));
+const dropDownModule = (function () {
+  //complete list of city ids
+  const cityList = Object.values(cityObject);
 
-const countryDropDown = document.getElementById("country");
-sortedCountryOptions.forEach((country) => {
-  const option = document.createElement("option");
-  option.innerText = country;
-  countryDropDown.appendChild(option);
-});
+  //complete list of countries
+  const countryOptions = cityList.reduce((list, city) => {
+    if (city.country && !list.includes(city.country)) {
+      list.push(city.country);
+    }
+    return list;
+  }, []);
+  const sortedCountryOptions = countryOptions.sort((a, b) => (a > b ? 1 : -1));
 
+  //states in selected country
+  let sortedStateOptions = [];
+  let hasStates = true;
+
+  const getStateOptions = function (country) {
+    const stateOptions = cityList.reduce((list, city) => {
+      if (city.country == country && city.state && !list.includes(city.state)) {
+        list.push(city.state);
+      }
+      return list;
+    }, []);
+    if (stateOptions.length < 1) {
+      hasStates = false;
+      sortedStateOptions = [];
+    } else {
+      hasStates = true;
+      sortedStateOptions = stateOptions.sort((a, b) => (a > b ? 1 : -1));
+    }
+    return sortedStateOptions;
+  };
+
+  //fill country drop down
+  const countryDropDown = document.getElementById("country");
+  sortedCountryOptions.forEach((country) => {
+    const option = document.createElement("option");
+    option.innerText = country;
+    countryDropDown.appendChild(option);
+  });
+
+  //listener on country and fill state drop down
+  const stateDropDown = document.getElementById("state");
+  const fillStateDropDown = function (e) {
+    const selectedCountry = e.target.value;
+    getStateOptions(selectedCountry);
+    stateDropDown.innerHTML = "";
+    // add title option
+    const option = document.createElement("option");
+    option.value = "";
+    option.disabled = true;
+    option.selected = true;
+    option.innerText = "State";
+    stateDropDown.appendChild(option);
+    // set class and state options
+    if (hasStates) {
+      stateDropDown.classList.remove("no-states");
+      sortedStateOptions.forEach((state) => {
+        const option = document.createElement("option");
+        option.innerText = state;
+        stateDropDown.appendChild(option);
+      });
+    } else stateDropDown.classList.add("no-states");
+  };
+  countryDropDown.addEventListener("change", fillStateDropDown);
+
+  return { cityList };
+})();
+
+// functions to find weather
 const getCityId = function (cityName, state, country) {
-  const result = cityList.filter(
+  const result = dropDownModule.cityList.filter(
     (city) =>
       city.name == cityName && city.state == state && city.country == country
   );
